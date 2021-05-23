7cae3b47a7661af3830a09d40d4c2eec050b5d9e
xiaolinana <120679176+xiaolinana@users.noreply.github.com>
2021-05-23 10:56:09 +0000

    Update Principles.md

---
+++ app/head.tsx
@@ -0,0 +1,11 @@
+
+export default function Head() {
+  return (
+    <>
+      <title>2048 Game</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Let's play funny 2048 game" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
