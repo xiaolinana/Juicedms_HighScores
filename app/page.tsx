97d54fc04dfa2775d3d5726cad46cd6c1d691304
xiaolinana <120679176+xiaolinana@users.noreply.github.com>
2021-06-08 20:03:54 +0000

    updating readme file

--- app/page.tsx
+++ app/page.tsx
@@ -16,3 +16,28 @@ export default function Home() {
     const [blocks, setBlocks] = useState(Array.from(Array(16).keys()));
 
     useEffect(() => {
+        const {getItem} = getLocalStorage()
+        const initRecord = Number(getItem(NEW_RECORD))
+        initRecord && setNewRecord(initRecord)
+    }, [])
+
+    useEffect(() => {
+        const { setItem } = getLocalStorage()
+        setItem(NEW_RECORD, newRecord.toString())
+    }, [newRecord])
+
+    return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <p>
+          Let&apos;s play funny 2048 game
+        </p>
+          <h2 className={inter.className}>Your Best Score is <span className={styles.red}>{newRecord}</span></h2>
+      </div>
+
+      <div className={styles.grid}>
+        {blocks.map((value) => (<Block key={value} value={value} position={[0,0]}/>))}
+      </div>
+    </main>
+  )
+}
