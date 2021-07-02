ad84dfa8384da431e5e298c5e43312a01df29c44
xiaolinana <120679176+xiaolinana@users.noreply.github.com>
2021-07-02 15:11:58 +0000

    language updates

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,2 +1,3 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiRespon
+import type { NextApiRequest, NextApiResponse } from 'next'
+
