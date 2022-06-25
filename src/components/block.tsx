02c4cefd6a92083df11ad7d499b898c6c627ba22
xiaolinana <120679176+xiaolinana@users.noreply.github.com>
2022-06-25 15:37:19 +0000

    Remove unused global

--- src/components/block.tsx
+++ src/components/block.tsx
@@ -1,4 +1,4 @@
 import React, {useEffect, useState} from 'react';
 import styles from "@/app/page.module.css";
 import {usePrevProps} from "@/src/hooks/use-prev-props";
-import {useBoard} from "@
+import {useBoard} from "@/src/contexts/
