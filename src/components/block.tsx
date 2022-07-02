3525d8217c07ae520014d7c5cf79cd9948a3ca77
xiaolinana <120679176+xiaolinana@users.noreply.github.com>
2022-07-02 12:40:33 +0000

    Adding to readme

--- src/components/block.tsx
+++ src/components/block.tsx
@@ -2,4 +2,6 @@ import React, {useEffect, useState} from 'react';
 import styles from "@/app/page.module.css";
 import {usePrevProps} from "@/src/hooks/use-prev-props";
 import {useBoard} from "@/src/contexts/board";
-import invariant from "tiny-invaria
+import invariant from "tiny-invariant";
+
+interface BlockPro
