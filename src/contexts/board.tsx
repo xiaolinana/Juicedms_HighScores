56486b70800075decd29757810c6d48e1c94a08c
xiaolinana <120679176+xiaolinana@users.noreply.github.com>
2022-10-25 11:24:14 +0000

    removed deprecated functions

--- src/contexts/board.tsx
+++ src/contexts/board.tsx
@@ -17,4 +17,4 @@ export const BoardProvider = ({
                                   children,
                                   containerWidth = 0,
                                   tileCount = 4,
-   
+                         
