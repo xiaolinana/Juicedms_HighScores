fa02eaff4d288362699b12e582ba9ee898dd237e
xiaolinana <120679176+xiaolinana@users.noreply.github.com>
2023-01-10 22:57:24 +0000

    keyboard shortcuts

--- src/reducer/game.ts
+++ src/reducer/game.ts
@@ -28,3 +28,28 @@ export const GameReducer = (state: State, action: Action) => {
     switch (action.type) {
         case "CREATE_TILE":
             return {
+                ...state,
+                tiles: {
+                    ...state.tiles,
+                    [action.tile.id]: action.tile,
+                },
+                byIds: [...state.byIds, action.tile.id],
+                hasChanged: false,
+            };
+        case "UPDATE_TILE":
+            return {
+                ...state,
+                tiles: {
+                    ...state.tiles,
+                    [action.tile.id]: action.tile,
+                },
+                hasChanged: true,
+            };
+        case "MERGE_TILE":
+            const {
+                [action.source.id]: source,
+                [action.destination.id]: destination,
+                ...restTiles
+            } = state.tiles;
+            return {
+                ...state,
